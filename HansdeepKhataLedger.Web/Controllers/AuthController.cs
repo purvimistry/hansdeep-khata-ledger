607ca279dd77a5ca919a0b3b9f62df26cc0349d4
Purvi Mistry <purvi10feb@gmail.com>
2026-05-14 19:11:20 +0530

    Create initial custom admin login page structure

---
+++ HansdeepKhataLedger.Web/Controllers/AuthController.cs
@@ -0,0 +1,25 @@
+﻿using HansdeepKhataLedger.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HansdeepKhataLedger.Web.Controllers
+{
+    public class AuthController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(LoginViewModel model)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            return RedirectToAction("Index", "Dashboard");
+        }
+    }
+}
---
+++ HansdeepKhataLedger.Web/Models/LoginViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace HansdeepKhataLedger.Web.Models
+{
+    public class LoginViewModel
+    {
+        public string Username {  get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
