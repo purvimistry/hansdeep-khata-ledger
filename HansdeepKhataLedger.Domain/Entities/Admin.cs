174208f0ff985b39c0b5a0c5d9fbc3e6d328bdbe
Purvi Mistry <purvi10feb@gmail.com>
2026-05-16 11:55:14 +0530

    Add Admin entity in domain layer

---
+++ HansdeepKhataLedger.Domain/Entities/Admin.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HansdeepKhataLedger.Domain.Entities
+{
+    public class Admin
+    {
+        public int Id { get; set; } 
+        public string Username { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
+        public bool IsActive { get; set; } = true;
+        public DateTime CreatedOn {  get; set; } = DateTime.UtcNow;
+    }
+}
